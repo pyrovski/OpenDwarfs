8ab4c6a90cde12c613a64df1ca19824723378b84
Peter Bailey <peter.eldridge.bailey@gmail.com>
2013-04-16 18:42:55 -0400

    remove dependency on missing file

--- combinational-logic/crc/Makefile.mk
+++ combinational-logic/crc/Makefile.mk
@@ -10,7 +10,7 @@ crc_LDFLAGS = -lm
 crc_SOURCES = combinational-logic/crc/crc_algo.c
 
 all_local += dwarf-crc-all-local
-exec_local += dwarf-crc-exec-local
+#exec_local += dwarf-crc-exec-local
 
 dwarf-crc-all-local:
 	cp $(top_srcdir)/combinational-logic/crc/crc_algo_kernel.cl .
